189d31eb82d58d8deaf428db145491bffa1ca3de
Sidhant <sidhantm.ic.17@nsit.net.in>
2021-01-17 18:35:59 +0530

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Sidhant Madan on 17/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
