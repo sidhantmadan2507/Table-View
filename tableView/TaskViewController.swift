9b595c7dd578bf62d5df89a659267b6b918ff3d1
Sidhant <sidhantm.ic.17@nsit.net.in>
2021-01-17 20:21:28 +0530

    Implemented table view

---
+++ tableView/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  tableView
+//
+//  Created by Sidhant Madan on 17/01/21.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
